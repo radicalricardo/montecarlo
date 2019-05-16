e319eebaf05db63b0969d4f01dc9a1628efef796
Luís Silvério <oppositedeep@gmailcom>
2019-05-16 11:59:21 +0100

    Codigo single thread + contagem de tempo de execução

---
+++ monteCarlo.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+#define SEED 666
+
+
+void montecarlo(){
+
+
+   int numIteracoes=0;
+   double x,y;
+   int i,count=0; /* # of points in the 1st quadrant of unit circle */
+   double z;
+   double pi;
+
+   printf("Por favor insira o número de tentativas: ");
+   scanf("%d",&numIteracoes);
+
+   /* initialize random numbers */
+   srand(SEED);
+   count=0;
+   for ( i=0; i<numIteracoes; i++) {
+      x = (double)rand()/RAND_MAX;
+      y = (double)rand()/RAND_MAX;
+      z = x*x+y*y;
+      if (z<=1) count++;
+      }
+   pi=(double)count/numIteracoes*4;
+   printf("Número de Tentativas: %d , a estimativa do pi é %g \n",numIteracoes,pi);
+}
+
+int main() {
+clock_t start = clock();
+
+montecarlo();
+
+clock_t stop = clock();
+double elapsed = (double)(stop - start) * 1000.0 / CLOCKS_PER_SEC;
+printf("Time elapsed in ms: %f", elapsed);
+}
