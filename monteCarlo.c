fdd2e63a78a9523540d3d92bfda9fc7bad21bf2d
Luís Silvério <oppositedeep@gmailcom>
2019-05-16 12:05:58 +0100

    Alterações à estrutura do codigo

--- monteCarlo.c
+++ monteCarlo.c
@@ -8,12 +8,12 @@
 
 void montecarlo(){
 
+clock_t start = clock();
 
    int numIteracoes=0;
-   double x,y;
+   double x,y, z, pi;
    int i,count=0; /* # of points in the 1st quadrant of unit circle */
-   double z;
-   double pi;
+
 
    printf("Por favor insira o número de tentativas: ");
    scanf("%d",&numIteracoes);
@@ -27,16 +27,18 @@ void montecarlo(){
       z = x*x+y*y;
       if (z<=1) count++;
       }
+      
    pi=(double)count/numIteracoes*4;
    printf("Número de Tentativas: %d , a estimativa do pi é %g \n",numIteracoes,pi);
-}
-
-int main() {
-clock_t start = clock();
-
-montecarlo();
 
 clock_t stop = clock();
 double elapsed = (double)(stop - start) * 1000.0 / CLOCKS_PER_SEC;
 printf("Time elapsed in ms: %f", elapsed);
+}
+
+int main() {
+
+montecarlo();
+
+
 }
